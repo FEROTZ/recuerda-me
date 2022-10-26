554ed7765e4d96972356791d215a7b15d94cf35f
ricardo gama <richard72cruspe@gmail.com>
2022-10-25 20:18:15 -0500

    cambios del menu

--- resources/views/landingpage/servicio/servicios.blade.php
+++ resources/views/landingpage/servicio/servicios.blade.php
@@ -25,119 +25,30 @@
     <link rel="stylesheet" href="{{ asset('resources/landing-assets/css/default.css')}}">
     <!--====== Style css ======-->
     <link rel="stylesheet" href="{{ asset('resources/landing-assets/css/style.css')}}">
+
     <link rel="stylesheet" href="{{ asset('resources/landing-asset/css/style servicios.css')}}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel='stylesheet'>
+
     <script src="https://kit.fontawesome.com/e65f2cf6ae.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <header id="home" class="header-area pt-100">
-        <div class="navigation-bar desktop">
-            <div class="container pa-x">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <nav class="navbar navbar-expand-lg">
-                            <a class="navbar-brand mr-5" href="{{ route('home')}}">
-                                <img src="{{ asset('resources/logo/recuerdame-logo.png') }}" alt="Logo">
-                            </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="toggler-icon"></span>
-                                <span class="toggler-icon"></span>
-                                <span class="toggler-icon"></span>
-                            </button>
-                            <!-- Search bar START -->
-                            <div class="search-bar">
-                                <div class="inputs">
-                                    <i class="fa fa-search"></i>
-                                    <input type="text" class="form-control " placeholder="Realizar una búsqueda...">
-                                </div>
-                            </div>
-                            <!-- Search bar ENDS -->
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <ul id="nav" class="navbar-nav ml-auto">
-                                    <li class="nav-item active">
-                                        <a class="page-scroll" href="{{ route('home')}}">Inicio</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('servicios')}}">Servicios</a>
-                                    </li>
-                                    <li class="nav-item dropdown">
-                                        <a class="page-scroll dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            Conócenos
-                                        </a>
-                                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                            <a class="dropdown-item" href="{{ route('nosotros')}}">¿Quiénes somos?</a>
-                                            <a class="dropdown-item" href="{{ route('misión')}}">Misión y visión </a>
-                                        </div>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('contacto')}}">Contacto</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll login" href="{{ route('login') }}"><i class="lni lni-user"></i></a>
-                                    </li>
-                                </ul> <!-- navbar nav -->
-                            </div>
-                        </nav>
-                         <!-- navbar -->
-                    </div>
-                </div> <!-- row -->
-            </div> <!-- container -->
-        </div> <!-- navigation bar desktop version -->
-        <div class="navigation-bar mobile display-none">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <nav class="navbar navbar-expand-lg">
-                            <a class="navbar-brand" href="{{ route('home')}}">
-                                <img src="{{ asset('resources/logo/recuerdame-logo.png')}}" alt="Logo">
-                            </a>
-                            <button class="navbar-toggler" type="button" data-toggle="collapse"
-                                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                                aria-expanded="false" aria-label="Toggle navigation">
-                                <span class="toggler-icon"></span>
-                                <span class="toggler-icon"></span>
-                                <span class="toggler-icon"></span>
-                            </button>
-                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                                <!-- Search bar START -->
-                            <div class="search-bar">
-                                <div class="inputs">
-                                    <i class="fa fa-search"></i>
-                                    <input type="text" class="form-control " placeholder="Realizar una búsqueda...">
-                                </div>
-                            </div>
-                            <!-- Search bar ENDS -->
-                                <ul id="nav" class="navbar-nav ml-auto">
-                                    <li class="nav-item active">
-                                        <a class="page-scroll" href="{{ route('home')}}">Inicio</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('servicios')}}">Servicios</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('nosotros')}}">¿Quiénes somos?</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('misión')}}">Misión y visión </a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll" href="{{ route('contacto')}}">Contacto</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="page-scroll login" href="{{ route('login')}}"><i class="lni lni-user"></i> Iniciar sesión</a>
-                                    </li>
-                                </ul> <!-- navbar nav -->
-                            </div>
-                        </nav> <!-- navbar -->
-                    </div>
-                </div> <!-- row -->
-            </div> <!-- container -->
-        </div> <!-- navigation bar mobile version -->
-    </header>
-
+    <header class="header" id="inicio">
+        
+        <div class="search-bar">
+            <div class="inputs">
+                <i class="fa fa-search"></i>
+                <input type="text" class="form-control " placeholder="Realizar una búsqueda...">
+            </div>
+        <img src="icons/Hamburguesa.svg" alt=""  class="hamburguer">
+        <nav class="menu-navegacion " >
+            <a href="{{ route('home')}}">Inicio</a>
+            <a href=="{{ route('servicios')}}">servicio</a>
+            <a href="{{ route('nosotros')}}">¿Quienes somos?</a>
+            <a href="{{ route('misión')}}">Misión y vision</a>
+            <a href="{{ route('contacto')}}">Contacto</a>
+        </nav>
     <!--====== SERVICES PART START ======-->
     <div class="container-fluid pt-5">
         <div class="container">
@@ -387,6 +298,8 @@
     <script src="{{ asset('resources/landing-assets/js/aos.js')}}"></script>
     <!--====== Main js ======-->
     <script src="{{ asset('resources/landing-assets/js/main.js')}}"></script>
+
+    <script src="{{ asset('resources/landing-assets/js/js/menu.js')}}"></script>
 
 </body>
 
