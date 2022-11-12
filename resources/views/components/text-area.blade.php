0e21e852620565b617b1d7f58055c87c52e73eac
Fernando Cortés <fernando.cortes971@gmail.com>
2022-11-12 10:39:31 -0600

    Fernando | Se creo componente de text area y se implemento

--- resources/views/components/text-area.blade.php
+++ resources/views/components/text-area.blade.php
@@ -3,10 +3,3 @@
   'border-gray-300 focus:border-zinc-900 focus:ring focus:ring-zinc-400 focus:ring-opacity-50 rounded-md shadow-sm'
 
 ]) !!}></textarea>
-                                            name="message" data-error="Por favor escriba su mensaje" required="required"></textarea>
-
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 
-  'border-gray-300 focus:border-zinc-900 focus:ring
-  focus:ring-zinc-400 focus:ring-opacity-50
-  rounded-md shadow-sm']) !!}>
