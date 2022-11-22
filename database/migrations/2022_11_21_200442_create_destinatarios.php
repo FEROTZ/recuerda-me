d3448af6cb280fcac691d98ad50b5d13ef0a075b
Kevin Brayan <kevinbrayan33o@gmail.com>
2022-11-21 20:13:20 -0600

    migración de las tablas de destinatario y servicio

---
+++ database/migrations/2022_11_21_200442_create_destinatarios.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('destinatarios', function (Blueprint $table) {
+            $table->integer ('id_destinatario');
+            $table->integer('id_cliente');
+            $table->text('num_pedido');
+            $table->text('correo');
+            $table->text('nombre_destinatario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('destinatarios');
+    }
+};
---
+++ database/migrations/2022_11_21_200619_create_servicos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->integer('id_producto');
+            $table->text('nom_producto');
+            $table->text('des_producto');
+            $table->integer('num_servicio');
+            $table->text('descripcion');
+            $table->text('precio_servicio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
